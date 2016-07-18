05046f24e6df862d3734e8c68b8c57591a30235b
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-07-18 15:47:02 -0600

    removed code smell from red/black

--- src/script.js
+++ src/script.js
@@ -2,16 +2,25 @@
 
 for (var i = 0; i < 64; i++) {
   var div = document.createElement('div');
+  div.style.width = "11.1%";
+  div.style.float = "left";
+  div.style.paddingBottom = "11.1%";
   if (i % 2 === 0) {
     div.style.backgroundColor = "red";
-    div.style.width = "11.1%";
-    div.style.float = "left";
-    div.style.paddingBottom = "11.1%";
   } else {
     div.style.backgroundColor = "black";
-    div.style.width = "11.1%";
-    div.style.float = "left";
-    div.style.paddingBottom = "11.1%";
   }
   document.body.appendChild(div)
 }
+
+//random colors
+
+// for (var i = 0; i < 64; i++) {
+//   var div = document.createElement('div');{
+//     div.style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+//     div.style.width = "11.1%";
+//     div.style.float = "left";
+//     div.style.paddingBottom = "11.1%";
+//   }
+//   document.body.appendChild(div)
+// }
